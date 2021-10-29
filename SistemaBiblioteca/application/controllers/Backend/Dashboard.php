a2fe876405fe99fdb83687a953430984e6fa86e2
seidy <seidymartinezg7@miumg.edu.gt>
2021-10-28 22:23:38 -0600

    actualization

---
+++ SistemaBiblioteca/application/controllers/Backend/Dashboard.php
@@ -0,0 +1,25 @@
+xml_error_string<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("Usuarios_model");
+        if (!$this->session->userdata("login")) {
+            redirect(base_url() . "cpanel");
+        }
+    }
+
+    public function index()
+    {
+        $data = array(
+            'title'     => 'Operaciones',
+            'contenido' => $this->load->view('backend/dashboard', '', true),
+        );
+
+        $this->load->view('backend/template', $data);
+    }
+}
